a2a3a0ef5e40bd45b7cb4ae3d23cc9b7008b2ff9
Shubham <shubhamnikumbh32@gmail.com>
2023-02-01 19:45:40 +0530

    Committed

--- Data.js
+++ Data.js
@@ -1,15 +1,34 @@
 
 
- var add = ["Shubham","Nikumbh","Rahul","Prasad"];
+//  var add = ["Shubham","Nikumbh","Rahul","Prasad"];
 
 
- var sum  = add.length;
+//  var sum  = add.length;
+
+//  console.log(sum);
+
+
+//  var sum = [23,34,45,67];
+
+//  var data = sum.map ((item) => item * 2);
+
+//  console.log(data);
+ 
+
+
+ let add = [20,23,45,67,78];
+
+ var sum = add.splice(1,3);
+
+ var side = add.slice(0,3);
 
  console.log(sum);
 
+console.log(side);
 
- var sum = [23,34,45,67];
 
- var data = sum.map ((item) => item * 2);
 
- console.log(data);
+let play  = [20,45,67,89,...sum] ;
+
+console.log(play)
+
