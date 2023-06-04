2b958312573ce38e37c6e123f38ede0056ec37a1
Shubham <shubhamnikumbh32@gmail.com>
2023-06-04 11:15:29 +0530

    3rd

--- Practice.js
+++ Practice.js
@@ -1,16 +1,25 @@
 
 
- let arr =[1,2,3,4,5]
+//  let arr =[1,2,3,4,5]
 
- let n=3;
+//  let n=3;
 
- function rotate (arrValue,num){
-    for (let index = 0; index < num; index++) {
-        let lastitem = arrValue.pop()
-        arrValue.unshift(lastitem)
-        console.log("result",arrValue)
-    }
- }
+//  function rotate (arrValue,num){
+//     for (let index = 0; index < num; index++) {
+//         let lastitem = arrValue.pop()
+//         arrValue.unshift(lastitem)
+//         console.log("result",arrValue)
+//     }
+//  }
 
- rotate(arr,n)
-  
+//  rotate(arr,n)
+
+   var data ="Shubham" ;
+   var add="Shubha"
+
+   if (data===add) {
+    console.log("Same")
+   } else {
+    console.log("different")
+   }
+
