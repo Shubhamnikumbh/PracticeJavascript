26d96e4b6e87a284ba1f25a4e5be618b6dbe09ef
Shubham <shubhamnikumbh32@gmail.com>
2023-06-04 16:59:37 +0530

    5th

--- Practice.js
+++ Practice.js
@@ -13,20 +13,37 @@
 //  }
 
 //  rotate(arr,n)
-  let arr =['aaa','bbb','ccc','aaa','bbb'];
 
-  let obj ={};
+//   let arr =['aaa','bbb','ccc','aaa','bbb'];
 
-  for (let index = 0; index < arr.length; index++) {
+//   let obj ={};
+
+//   for (let index = 0; index < arr.length; index++) {
     
-   let item = arr[index] ;
+//    let item = arr[index] ;
 
-   if (obj[item]) {
-    obj[item]= obj[item] + 1 ;
-   } else {
-    obj[item]= 1 ;
-   }
+//    if (obj[item]) {
+//     obj[item]= obj[item] + 1 ;
+//    } else {
+//     obj[item]= 1 ;
+//    }
     
-  }
+//   }
 
-  console.log("result" ,obj)
+//   console.log("result" ,obj)
+
+
+    let data = [10,12,4,,58,35,27,94,58];
+     
+     let max= data[0];
+
+    for (let index = 1; index < data.length; index++) {
+        
+       
+        if (data[index] > max) {
+          max = data[index];
+        }
+
+      
+    }
+     console.log(max)
